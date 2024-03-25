baf1e38016a5380be102bc0a81f8890937af54be
Marat-FMK <kosjachok8@yandex.ru>
2024-03-26 02:25:57 +0300

    Initial Commit

---
+++ lighter 2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lighter 2024
+//
+//  Created by Marat_FMK on 26.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
